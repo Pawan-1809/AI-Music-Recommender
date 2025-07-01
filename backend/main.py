222542597367d9d7ae594de3d1cc8b453c755551
Pawan kumar <pawankr1623114@gmail.com>
2025-07-01 12:43:49 +0530

    Minor fix in ports

--- backend/main.py
+++ backend/main.py
@@ -67,3 +67,8 @@ def predict_mood(req: MoodRequest):
     except Exception as e:
         return {"error": str(e)}
 
+if _name_ == "_main_":
+    import os
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
